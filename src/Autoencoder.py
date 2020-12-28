f19418326298ca272ddc100a3b99cd42e5a57b41
Daniil Kuznetsov <dkuznetsov17@gmail.com>
2020-12-28 17:11:14 +0000

    Add everything to repo

---
+++ src/Autoencoder.py
@@ -0,0 +1,59 @@
+import torch as t
+
+class Autoencoder(t.nn.Module):
+    def __init__(self, input_shape=784):
+        super().__init__()
+
+        self.layer1 = t.nn.Linear(input_shape, 100)
+        t.nn.init.kaiming_uniform_(self.layer1.weight, nonlinearity="relu")
+        t.nn.init.zeros_(self.layer1.bias)
+        
+        self.layer2 = t.nn.Linear(100, 50)
+        t.nn.init.kaiming_uniform_(self.layer2.weight, nonlinearity="relu")
+        t.nn.init.zeros_(self.layer2.bias)
+
+        self.layer3 = t.nn.Linear(50, input_shape)
+        t.nn.init.kaiming_uniform_(self.layer3.weight, nonlinearity="relu")
+        t.nn.init.zeros_(self.layer3.bias)
+
+        self.loss = t.nn.BCELoss(reduction="sum")
+        self.opti = t.optim.Adam(self.parameters(), lr=0.001)
+
+    # Returns the output of the final layer, of the forward propogation
+    def forward(self, x):
+        self.train()
+
+        x = t.relu(self.layer1(x))
+        x = t.relu(self.layer2(x))
+        x = t.sigmoid(self.layer3(x))
+        return x
+
+    # Uses loss of the forward propogation to train network
+    def backward(self, pred, y):
+        # Calculate the loss and calculate grads
+        loss = self.loss(pred, y)
+        loss.backward()
+        # Apply grads to weights
+        self.opti.step()
+        self.opti.zero_grad()
+
+        return loss.item()
+
+    # Forward propogate without autograd
+    def predict(self, x):
+        self.eval()
+        with t.no_grad():
+            x = self(x)
+            return x
+
+    def evaluate(self, x, y):
+        return self.loss(self.predict(x), y).item()
+
+    # Save model params
+    def save(self, path):
+        t.save(self.state_dict(), path)
+
+    # Load saved model params
+    def load(self, path):
+        self.load_state_dict(t.load(path))
+
---
+++ train.py
@@ -0,0 +1,39 @@
+from src.Autoencoder import Autoencoder
+import torchvision as tv
+import torch as t
+
+NUM_EPOCHS = 1
+SAVE_PATH = "saved_models/autoencoder.pt"
+BATCH_SIZE = 1000
+
+
+if __name__ == "__main__":
+    device = t.device("cuda" if t.cuda.is_available() else "cpu")
+
+    training_data = tv.datasets.MNIST("data/", train=True, download=True, transform=tv.transforms.ToTensor())
+    training_data = t.utils.data.DataLoader(training_data, batch_size=BATCH_SIZE, shuffle=True) 
+    test_data = tv.datasets.MNIST("data/", train=False, download=True, transform=tv.transforms.ToTensor())
+    test_data = t.utils.data.DataLoader(test_data, batch_size=BATCH_SIZE, shuffle=True)
+
+    net = Autoencoder()
+    net.to(device)
+
+    for epoch in range(NUM_EPOCHS):
+        for (x_batch, _) in training_data:
+            x_batch = t.flatten(x_batch, 1).to(device)
+            for count, x in enumerate(x_batch):
+                err = net.backward(net(x), x)
+                if not (count % 100):
+                    print("Cross-Entropy error: {}".format(err))
+    
+    error_sum = 0
+    num = 0
+    for (x_batch, _) in test_data:
+        num += x_batch.shape[0]
+        x_batch = t.flatten(x_batch, 1).to(device)
+        for x in x_batch:
+            error_sum += net.evaluate(net(x), x)
+
+    net.save(SAVE_PATH)
+    print("Avg Cross-Entropy testing error: {}".format(error_sum/num))
+    
